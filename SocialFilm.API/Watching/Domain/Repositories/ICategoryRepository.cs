a76d78a6290b0b596b5f2dd9814258dbfb18fbff
EnzoLivelli2000 <58576353+EnzoLivelli2000@users.noreply.github.com>
2022-08-08 15:49:54 -0500

    first commit: (feat) Adding Repositories Domain

---
+++ SocialFilm.API/Watching/Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using SocialFilm.API.Watching.Domain.Models;
+
+namespace SocialFilm.API.Watching.Domain.Repositories;
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> ListAsync();
+    Task AddAsync(Category category);
+    Task<Category> FindByIdAsync(int id);
+    Task<Episode> FindByNameAsync(string name);
+    void Update(Category category);
+    void Remove(Category category);
+}
---
+++ SocialFilm.API/Watching/Domain/Repositories/IEpisodeRepository.cs
@@ -0,0 +1,13 @@
+﻿using SocialFilm.API.Watching.Domain.Models;
+
+namespace SocialFilm.API.Watching.Domain.Repositories;
+
+public interface IEpisodeRepository
+{
+    Task<IEnumerable<Episode>> ListAsync();
+    Task AddAsync(Episode episode);
+    Task<Episode> FindByIdAsync(int episodeId);
+    Task<Episode> FindByTitleAsync(string title);
+    void Update(Episode episode);
+    void Remove(Episode episode);
+}
